4865fa914685f6a48eaa0e51f682874551011fcf
Guti <gustavo.costanti.lara@gmail.com>
2020-12-14 20:31:24 -0300

    Third Commit

--- Memory Game/assets/scripts/script.js
+++ Memory Game/assets/scripts/script.js
@@ -1,50 +1,59 @@
 const FRONT = "card-front";
-const BACK = "card_back";
+const BACK = "card-back";
+const CARD = "card";
+const ICON = "icon";
 
-let techs = ['bootstrap', 'css', 'electron', 'firebase', 'html', 'javascript', 'jquery', 'mongo', 'node', 'react'];
-
-let cards = null;
 startGame();
 
 function startGame() {
-    cards = createCardsFromTechs(techs);
-    suffleCards(cards);
-    console.log(cards);
+    initializeCards(game.createCardsFromTechs());
 }
 
-function suffleCards(cards) {
-    let currentIndex = cards.length;
-    let randomIndex = 0;
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+    game.cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        createCardContent(card, cardElement);
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+}
 
-    while(currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
 
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if (face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
     }
+    element.appendChild(cardElementFace);
 }
 
-createCardsFromTechs(techs);
-function createCardsFromTechs(techs) {
-    let cards = [];
-    for (let tech of techs) {
-        cards.push(createPairFromTech(tech));
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add("flip");
+        if (game.checkMatch()) {
+            game.clearCards();
+        } else {
+            setTimeout(() => {
+                let firstCardView = document.getElementById(game.firstCard.id);
+                let secondCardView = document.getElementById(game.secondCard.id);
+                firstCardView.classList.remove('flip');
+                secondCardView.classList.remove('flip');
+                game.clearCards();
+            }, 1000);
+        }
     }
-    return cards.flatMap(pair => pair);
-}
-
-function createPairFromTech(tech) {
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-
-function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 1000);
 }
