6fc9c1768ab254a46b2c340576ee8eec5dff2a8c
Guti <gustavo.costanti.lara@gmail.com>
2020-12-16 21:56:25 -0300

    My First Web Server

--- First WebServer with nodejs/index.js
+++ First WebServer with nodejs/index.js
@@ -1,9 +1,27 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer((request, response) => {
 
-    response.writeHead(200, {'content-type': 'text/plain'});
-    response.end("Hello World!");
+    let path = url.parse(request.url).pathname;
+    
+    if (path == "" || path == "/") {
+        path = "/index.html";
+    }
+
+    let fileName = "." + path;
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            response.writeHead(404, { "Content-Type": "text/html; charset=utf8" });
+            response.end("<h1>Página não encontrada</h1>");
+        } else {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write(data);
+            response.end();
+        }
+    })
 
 }).listen(3000, (err) => {
     if (err) {
