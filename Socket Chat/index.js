b5cf4a4570664cd312e67f7ab48b5c925b48a908
Guti <gustavo.costanti.lara@gmail.com>
2021-01-03 16:09:48 -0300

    Chat 01

--- Socket Chat/index.js
+++ Socket Chat/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const socketIo = require('socket.io');
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Running");
+})
+
+const messages = []
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New Connection');
+    socket.emit('update_messages', messages)
+
+    socket.on('new_message', (data) => {
+        messages.push(data.msg)
+
+        io.emit('update_messages', messages)
+    })
 })
